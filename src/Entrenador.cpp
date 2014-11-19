0ce3c721baa16168f70f0c935eedcc3d7a87657e
Deakinson <brauliopuentes1995@hotmail.es>
2014-11-19 17:09:06 -0300

    cosillas

--- src/Entrenador.cpp
+++ src/Entrenador.cpp
@@ -1,5 +1,10 @@
 #include "Entrenador.h"
-#include <fstream.h>
+#include "Pokemon.h"
+#include <fstream>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
 
 Entrenador::Entrenador()
 {
@@ -11,7 +16,17 @@ Entrenador::~Entrenador()
     //dtor
 }
 
-Pokemon Entrenador::setEquipo()
-{
-
+Pokemon Entrenador::setEquipo(){
+    char buffer[128];
+    char *buf;
+    Pokemon all[151];
+    ifstream puchamon;
+    puchamon.open("lista_pokemon.txt");
+    if (puchamon.is_open()){
+        while(!puchamon.eof()){
+            puchamon.getline(buffer,128);
+            buf=strtok(buffer," ");
+            buf=strtok(NULL," ");
+        }
+    }
 }
