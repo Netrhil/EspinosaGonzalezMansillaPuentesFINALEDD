6145e91ec5c38bd429264be4d019fae46bc26c12
Haylin Ellenes <haylin-chama@hotmail.es>
2014-11-19 17:35:11 -0300

    Update Entrenador.cpp
    traspasando desde python

--- src/Entrenador.cpp
+++ src/Entrenador.cpp
@@ -16,17 +16,40 @@ Entrenador::~Entrenador()
     //dtor
 }
 
+Entrenador::SetNombre()
+{
+    
+}
+
 Pokemon Entrenador::setEquipo(){
-    char buffer[128];
-    char *buf;
-    Pokemon all[151];
-    ifstream puchamon;
-    puchamon.open("lista_pokemon.txt");
-    if (puchamon.is_open()){
-        while(!puchamon.eof()){
-            puchamon.getline(buffer,128);
-            buf=strtok(buffer," ");
-            buf=strtok(NULL," ");
-        }
-    }
+    arch="Equipo"+str(n)+".txt";
+    usuario=open(arch,"w")
+    print "\n Su equipo de ", n_poke_eq, " pokemons es:\n"
+    i=0
+    while i!=n_poke_eq:
+        if n==1:
+            npoke=raw_input("Nombre Pokemon que quiere: ")
+            archivo=open("lista_pokemon.txt", "r")
+            a=False
+            for linea in archivo:
+                dato=linea.split(" ")
+                if dato[1]==npoke:
+                    print "Pokemon",i+1, ": ",dato[1],'\n'
+                    usuario.write(linea)
+                    a=True
+            if a==False:
+                print "Pokemon no valido, ingrese pokemon",i
+                i=i-1
+            i=i+1
+            archivo.close()
+        if n==2:
+            num_p=random.randint(1, 151)
+            archivo=open("lista_pokemon.txt", "r")
+            for linea in archivo:
+                dato=linea.split(" ")
+                if int(dato[0])==num_p:                    
+                    print "Pokemon",i+1, ": ",dato[1] ##Borar Linea al entregar
+                    usuario.write(linea)
+            archivo.close()
+            i=i+1
 }
